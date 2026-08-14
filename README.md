d29321a9a3fd80b3bcbde2fbbc7ebfa0141b7bb8
salah137 <boksaloh@gmail.com>
2026-08-14 19:50:36 +0100

    quite finished, to be continued

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # arm-cortex-M3-round-robin-scheduler
+
+- this is a lightweight bare metal round roubine scheduler and context switcher for arm cortex M3
+- it uses a Systick timer that fires a PendSv handler that perform the contex switching saving the old thread state (`R0–R12`, `LR`, `PC`, `xPSR`), and loading the next thread
+
+## What is a thread
+
+- this project represent threads in this format:
+
+```C
+      typedef struct thread {
+        char *name;
+        uint32_t id;
+        uint32_t *stack_top;
+        uint32_t *stack_end;
+        uint32_t stack_size;
+        void *params;
+        void (*thread_function)(void *);
+        struct thread *next_task;
+        } thread_t;
+```
+
+- each thread has its own stack that start at stack_top and descend til it reachs the end,
+- the stack_top starts at the end of the previos thread's stack,
+
+- threads are aligned in a linked list allocated in static bump heap (nothing fancy way to say a big  array)
+```C
+    uint8_t my_dump_heap[2048] __attribute__((aligned(8))) = {0};
+    ...
+    my_heap_t my_heap = {
+        .start = &my_dump_heap[0],
+        .end = &my_dump_heap[0] + sizeof(my_dump_heap),
+        .pos = &my_dump_heap[0],
+    };
+
+```
+
+## Prerequisites
+  - `qemu-system-arm` for emulating
+  - `arm-none-eabi-gcc` for compiling
+  - `arm-none-eabi-ld` for linking
+  -  `arm-none-eabi-gdb` for gdb debuguing
+
+## runing it
+- the programe QEMU arm-emulator, and GDB for debuging
+
+- first, compile and start the GDB server :
+``` shell
+    make qemu
+```
+
+- then listen in separate terminal :
+``` shell
+  make gdb
+```
--- main.c
+++ main.c
@@ -7,63 +7,91 @@
 #define SCB_SHPR3 (*((volatile uint32_t *)0xE000ED20UL))
 #define SCB_ICSR (*((volatile uint32_t *)0xE000ED04UL))
 
-
 extern int create_thread(char *name, void (*thread_function)(void *),
-                  uint32_t stack_size, void *params) ;
+                         uint32_t stack_size, void *params);
 extern void start_routines();
 extern void PendSv_handler(void);
 extern uint32_t *get_stack_pointer(char *name);
-extern thread_t* head;
+extern thread_t *head;
 
-void task1(void* param){
-    while (1) {
+void test_st(int i) { int x = i; }
 
-    }
+void task1(void *param) {
+  int i = 0;
+  while (1) {
+    __asm__ volatile("mov r4,  #0x41\n\t"
+                     "mov r5,  #0x51\n\t"
+                     "mov r6,  #0x61\n\t"
+                     "mov r8,  #0x81\n\t"
+                     "mov r9,  #0x91\n\t"
+                     "mov r10, #0xA1\n\t"
+                     "mov r11, #0xB1\n\t" ::
+                         : "r4", "r5", "r6",  "r8", "r9", "r10", "r11");
+  }
 }
 
-void task2(void* param){
-    while (1) {
-
-    }
+void task2(void *param) {
+  int i = 0;
+  while (1) {
+    __asm__ volatile("mov r4,  #0x42\n\t"
+                     "mov r5,  #0x52\n\t"
+                     "mov r6,  #0x62\n\t"
+                     "mov r8,  #0x82\n\t"
+                     "mov r9,  #0x92\n\t"
+                     "mov r10, #0xA2\n\t"
+                     "mov r11, #0xB2\n\t" ::
+                         : "r4", "r5", "r6", "r8", "r9", "r10", "r11");
+  }
 }
 
-void task3(void* param){
-    while (1) {
-
-    }
+void task3(void *param) {
+  int i = 0;
+  while (1) {
+      __asm__ volatile (
+                  "mov r4,  #0x66\n\t"
+                  "mov r5,  #0x66\n\t"
+                  "mov r6,  #0x66\n\t"
+                  "mov r7,  #0x66\n\t"
+                  "mov r8,  #0x66\n\t"
+                  "mov r9,  #0x92\n\t"
+                  "mov r10, #0xA122\n\t"
+                  "mov r11, #0x123\n\t"
+                  ::: "r4", "r5", "r6", "r8", "r9", "r10", "r11"
+              );
+  }
 }
 
-void setup(){
-    __asm__ volatile("cpsid i                 \n\t" : : : "memory");
+void setup() {
+  __asm__ volatile("cpsid i                 \n\t" : : : "memory");
 
-    create_thread("task1",task1, 128,(void *) NULL);
-    __asm__ volatile("cpsid i                 \n\t" : : : "memory");
+  create_thread("task1", task1, 128, (void *)NULL);
+  __asm__ volatile("cpsid i                 \n\t" : : : "memory");
 
-    create_thread("task2",task2, 128,(void *) NULL);
-    __asm__ volatile("cpsid i                 \n\t" : : : "memory");
+  create_thread("task2", task2, 128, (void *)NULL);
+  __asm__ volatile("cpsid i                 \n\t" : : : "memory");
 
-    create_thread("task3",task3, 128,(void *) NULL);
+  create_thread("task3", task3, 128, (void *)NULL);
 
-    __asm__ volatile("cpsie i                 \n\t" : : : "memory");
-
+  __asm__ volatile("cpsie i                 \n\t" : : : "memory");
 }
 
-int app_main(){
+int app_main() {
 
-    setup();
-    start_routines();
+  setup();
+  start_routines();
 
-    __asm__ volatile(
-    //    "svc #0\n\t"
-        "mov r0,#0\n\t"
-        
-        "msr psp,r0\n\t"
+  __asm__ volatile(
+      //    "svc #0\n\t"
+      "mov r0,#0\n\t"
 
-        : : :);
+      "msr psp,r0\n\t"
 
-    SCB_ICSR = ICSR_PENDSVSET_BIT;
+      :
+      :
+      :);
 
+  SCB_ICSR = ICSR_PENDSVSET_BIT;
 
-    while (1);
-
+  while (1)
+    ;
 }
